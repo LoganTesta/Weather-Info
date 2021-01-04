4c48f76e03df301916da57d871c2198c78c65a14
logan <logan.testa@outlook.com>
2021-01-04 15:32:14 -0800

    Add footer

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <link rel="icon" type="image/png" href="" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
               integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="assets/css/main-styles.css?mod=01012021" />
+        <link rel="stylesheet" type="text/css" href="assets/css/main-styles.css?mod=01042021" />
         <link rel="stylesheet" type="text/css" href="assets/css/print-styles.css?mod=12232020" media="print" />
     </head>
     <body class="page-index">
@@ -227,6 +227,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     </div>
                 </div>
             </div>
+            <footer class="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="footer__message">Copyright &copy; <?php echo date( "Y" ); ?> Weather Info.  All Rights Reserved.</div>
+                    </div>
+                </div>
+            </footer>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
